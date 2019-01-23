059788f21c292004ec4e65c7018fa15c481c52bc
Thiago Veronezi <thiago@veronezi.org>
2019-01-23 17:02:53 -0500

    basic tests

--- ft/src/main/java/todo/ft/Steps.java
+++ ft/src/main/java/todo/ft/Steps.java
@@ -1,24 +1,69 @@
 package todo.ft;
 
 import cucumber.api.java8.En;
+import lombok.SneakyThrows;
 import lombok.extern.java.Log;
+import lombok.val;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Log
 public class Steps implements En {
 
+    @SneakyThrows
+    private Connection getConnection() {
+        return DriverManager.getConnection("jdbc:postgresql://"
+                        + ServiceGetter.SERVICES.getDbHost() + ":"
+                        + ServiceGetter.SERVICES.getDbPort() + "/todo",
+                "todo_user", "todo_pass");
+    }
+
     @SuppressWarnings("unchecked")
     public Steps() {
         Given("the system is live", () -> {
-
+            try (val connection = getConnection()) {
+                val statement = connection.createStatement();
+                val resultSet = statement.executeQuery("SELECT 1");
+                resultSet.next();
+                resultSet.getInt(1);
+            }
         });
 
-        Given("the database is ready", () -> {
-
+        Given("I execute the '(.*)' update template (\\d+) times", (String sqlTemplate, Integer times) -> {
+            try (val connection = getConnection()) {
+                val stat = connection.createStatement();
+                for (int i = 0; i < times; i++) {
+                    stat.executeUpdate(String.format(sqlTemplate, i));
+                }
+            }
         });
 
-        Given("I execute the template '(.*)' (\\d+) times", (String sqlTemplate, Integer times) -> {
-
+        Given("I drop the '(.+)' table", (String string) -> {
+            try (val connection = getConnection()) {
+                val statement = connection.createStatement();
+                statement.executeUpdate("DROP TABLE IF EXISTS " + string);
+            }
         });
+
+        Given("I execute the '(.+)' update", (String string) -> {
+            try (val connection = getConnection()) {
+                val statement = connection.createStatement();
+                statement.executeUpdate(string);
+            }
+        });
+
+        Then("the count query '(.+)' returns (\\d+) rows", (String query, Integer expected) -> {
+            try (val connection = getConnection()) {
+                val statement = connection.createStatement();
+                val resultSet = statement.executeQuery(query);
+                resultSet.next();
+                assertThat(resultSet.getInt(1)).isEqualTo(expected);
+            }
+        });
+
 
     }
 }
--- ft/src/main/resources/todo/ft/todo.feature
+++ ft/src/main/resources/todo/ft/todo.feature
@@ -2,7 +2,9 @@ Feature: [Backend] Creating and updating todos
 
   Background:
     Given the system is live
-    And the database is ready
 
   Scenario: I create thousands of rows
-    Given I execute the template 'INSERT INTO names (name) VALUES (%d);' 10000 times
+    Given I drop the 'names' table
+    And I execute the 'CREATE TABLE names (name_id serial PRIMARY KEY, name CHAR (5000) NOT NULL);' update
+    And I execute the 'INSERT INTO names (name) VALUES ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb %d');' update template 10000 times
+    Then the count query 'SELECT count(*) FROM names' returns 10000 rows
